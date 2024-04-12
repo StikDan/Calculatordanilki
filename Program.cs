8cc0027dc7e6a94331ecf876a8916a0400d31997
StikDan <153372644+StikDan@users.noreply.github.com>
2024-04-12 20:27:47 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,34 @@
-﻿
 using System;
 
 namespace CalculatorCsharp
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static string value; private static int choice;
+
+        class Operations
         {
-
-            float firstNumber, secondNumber;
-            double result;
-            int choice;
-            string value;
-
-            do
+            public static double AdditionOperation(double numFirst, double numSecond)
             {
-
+                return numFirst + numSecond;
+            }
+            public static double SubtractionOperation(double numFirst, double numSecond)
+            {
+                return numFirst - numSecond;
+            }
+            public static double MultiplicationOperation(double numFirst, double numSecond)
+            {
+                return numFirst * numSecond;
+            }
+            public static double DivisionOperation(double numFirst, double numSecond)
+            {
+                return numFirst / numSecond;
+            }
+        }
+        class ProcessCalculation
+        {
+            public static void TextLines()
+            {
                 Console.WriteLine("======== КАЛЬКУЛЯТОР ==========");
                 Console.WriteLine("1. ОПЕРАЦИЯ сложения(+).");
                 Console.WriteLine("2. ОПЕРАЦИЯ вычитания(-).");
@@ -26,68 +39,62 @@ namespace CalculatorCsharp
                 Console.WriteLine("6. ОПЕРАЦИЯ возведение в квадрат(^).");
                 Console.WriteLine("7. ОПЕРАЦИЯ нахождения синуса(sin).");
                 Console.WriteLine("8. ОПЕРАЦИЯ нахождения косинуса(cos).");
+                Console.WriteLine("9. ОПЕРАЦИЯ нахождения тангенса(tg).");
                 Console.WriteLine("10. ОПЕРАЦИЯ логарифмирования(log).");
+            }
 
+            public static void UserInput()
+            {
                 Console.WriteLine("\nВВЕДИТЕ ПЕРВОЕ ЧИСЛО: ");
-                firstNumber = Convert.ToInt32(Console.ReadLine());
+                double numFirst = Convert.ToDouble(Console.ReadLine()); //обработка исключения при вводе символов
                 Console.WriteLine("\nВВЕДИТЕ ВТОРОЕ ЧИСЛО: ");
-                secondNumber = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("\nВЫБЕРИТЕ ОПЕРАТОР (1 - 10):- ");
+                double numSecond = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("\nВЫБЕРИТЕ ОПЕРАТОР (1 - 10): ", '\n');
+            }     
+        }
+
+        public static void Main()
+        {
+            do
+            {
+                ProcessCalculation processCalculation = new ProcessCalculation();
+                Operations operations = new Operations();
+
+                ProcessCalculation.TextLines();
+                ProcessCalculation.UserInput();
+
 
                 choice = Convert.ToInt32(Console.ReadLine());
 
-
                 switch (choice)
                 {
+                    default:
+                        Console.WriteLine("Неизвестный оператор!"); 
+                        break;
                     case 1:
-                        result = firstNumber + secondNumber;
-                        Console.WriteLine("Результат: " + result);
+                        Console.WriteLine("Результат: " + Operations.AdditionOperation, '\n');
                         break;
                     case 2:
-                        result = firstNumber - secondNumber;
-                        Console.WriteLine("Результат: " + result);
+                        Console.WriteLine("Результат: " + Operations.SubtractionOperation, '\n');
                         break;
                     case 3:
-                        result = firstNumber * secondNumber;
-                        Console.WriteLine("Результат: " + result);
+                        Console.WriteLine("Результат: " + Operations.MultiplicationOperation, '\n');
                         break;
                     case 4:
-                        result = firstNumber / secondNumber;
-                        Console.WriteLine("Результат: " + result);
+                        Console.WriteLine("Результат: " + Operations.DivisionOperation,'\n');  //(numFirst: processCalculation, numSecond: processCalculation));
                         break;
-                    case 5://кв корень
-                        result = Math.Sqrt(firstNumber);
-                        Console.WriteLine("Результат: " + result);
-                        break;
-                    case 6: //возвышение в степень (a-значение, b-степень)
-                        result = Math.Pow(firstNumber, 2);
-                        Console.WriteLine("Результат: " + result);
-                        break;
-                    case 7: //sin
-                        result = Math.Sin(firstNumber);
-                        Console.WriteLine("Результат: " + result);
-                        break;
-                    case 8: //cos
-                        result = Math.Cos(firstNumber);
-                        Console.WriteLine("Результат: " + result);
-                        break;
-                    case 9: //tg
-                        result = Math.Tan(firstNumber);
-                        Console.WriteLine("Результат: " + result);
-                        break;
-                    case 10:
-                        result = Math.Log(firstNumber);
-                        Console.WriteLine("Результат: " + result);
-                        break;
+                        //больше операций 
                 };
 
-                Console.Write("Вы хотите продолжить? (д/н):");
+                Console.Write("Вы хотите продолжить? (д/н):", '\n');
                 value = Console.ReadLine();
-
-
             }
-            while (value == "y" || value == "Y" || value == "д" || value == "Д");
+            while
 
+            (value == "y" || value == "Y" || value == "д" || value == "Д");
         }
     }
 }
+
+          
+                
