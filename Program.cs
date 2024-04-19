c5d03801961b1821418b074881e401a538ec524c
StikDan <153372644+StikDan@users.noreply.github.com>
2024-04-19 23:02:35 +0400

    Program.cs

--- Program.cs
+++ Program.cs
@@ -4,97 +4,56 @@ namespace CalculatorCsharp
 {
     public class Program
     {
-        private static string value; private static int choice;
-
-        class Operations
+        public static void Menu(Operation[] operations)
         {
-            public static double AdditionOperation(double numFirst, double numSecond)
+            Console.WriteLine("======== КАЛЬКУЛЯТОР ==========");
+            for (int i = 0; i < operations.Length; i++)
             {
-                return numFirst + numSecond;
-            }
-            public static double SubtractionOperation(double numFirst, double numSecond)
-            {
-                return numFirst - numSecond;
-            }
-            public static double MultiplicationOperation(double numFirst, double numSecond)
-            {
-                return numFirst * numSecond;
-            }
-            public static double DivisionOperation(double numFirst, double numSecond)
-            {
-                return numFirst / numSecond;
+                Operation operation = operations[i];
+                Console.WriteLine($"{i + 1}. ОПЕРАЦИЯ {operation.Name}");
             }
         }
-        class ProcessCalculation
+       public static void UserProgramInput()
         {
-            public static void TextLines()
-            {
-                Console.WriteLine("======== КАЛЬКУЛЯТОР ==========");
-                Console.WriteLine("1. ОПЕРАЦИЯ сложения(+).");
-                Console.WriteLine("2. ОПЕРАЦИЯ вычитания(-).");
-                Console.WriteLine("3. ОПЕРАЦИЯ умножения(*).");
-                Console.WriteLine("4. ОПЕРАЦИЯ деления(/).");
-                Console.WriteLine("4. ОПЕРАЦИЯ деления(/).");
-                Console.WriteLine("5. ОПЕРАЦИЯ вычисления корня(>).");
-                Console.WriteLine("6. ОПЕРАЦИЯ возведение в квадрат(^).");
-                Console.WriteLine("7. ОПЕРАЦИЯ нахождения синуса(sin).");
-                Console.WriteLine("8. ОПЕРАЦИЯ нахождения косинуса(cos).");
-                Console.WriteLine("9. ОПЕРАЦИЯ нахождения тангенса(tg).");
-                Console.WriteLine("10. ОПЕРАЦИЯ логарифмирования(log).");
-            }
+            var udi = new DataInput();
+            udi.UserInput();
+        }
 
-            public static void UserInput()
-            {
-                Console.WriteLine("\nВВЕДИТЕ ПЕРВОЕ ЧИСЛО: ");
-                double numFirst = Convert.ToDouble(Console.ReadLine()); //обработка исключения при вводе символов
-                Console.WriteLine("\nВВЕДИТЕ ВТОРОЕ ЧИСЛО: ");
-                double numSecond = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("\nВЫБЕРИТЕ ОПЕРАТОР (1 - 10): ", '\n');
-            }     
+        public static void StartApplication()
+        {
+            var app = new Application();
+            app.Run();
         }
 
         public static void Main()
+    {
+        try
         {
-            do
-            {
-                ProcessCalculation processCalculation = new ProcessCalculation();
-                Operations operations = new Operations();
-
-                ProcessCalculation.TextLines();
-                ProcessCalculation.UserInput();
-
-
-                choice = Convert.ToInt32(Console.ReadLine());
-
-                switch (choice)
-                {
-                    default:
-                        Console.WriteLine("Неизвестный оператор!"); 
-                        break;
-                    case 1:
-                        Console.WriteLine("Результат: " + Operations.AdditionOperation, '\n');
-                        break;
-                    case 2:
-                        Console.WriteLine("Результат: " + Operations.SubtractionOperation, '\n');
-                        break;
-                    case 3:
-                        Console.WriteLine("Результат: " + Operations.MultiplicationOperation, '\n');
-                        break;
-                    case 4:
-                        Console.WriteLine("Результат: " + Operations.DivisionOperation,'\n');  //(numFirst: processCalculation, numSecond: processCalculation));
-                        break;
-                        //больше операций 
-                };
-
-                Console.Write("Вы хотите продолжить? (д/н):", '\n');
-                value = Console.ReadLine();
-            }
-            while
-
-            (value == "y" || value == "Y" || value == "д" || value == "Д");
+            Menu([
+                new Addition(),
+                    new Substraction(),
+                    new Multiplacation(),
+                    new Multiplacation(),
+                    new Sqrt()
+                ]);
+           UserProgramInput();
+           StartApplication();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
         }
     }
-}
+  }
+} 
 
-          
-                
+        
+
+
+    
+
+
+
+
+
+
