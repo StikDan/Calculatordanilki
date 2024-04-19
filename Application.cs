577e81605b1c8da34e7daf2bef4ef9c9330fcd43
StikDan <153372644+StikDan@users.noreply.github.com>
2024-04-19 23:04:15 +0400

    Add files via upload

---
+++ Application.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorCsharp
+{
+    public class Application
+    {
+        private static string value; private static int choice;
+
+
+        public void Run()
+        {
+            do
+            {
+                Operation add = new Addition();
+                Operation sub = new Substraction();
+                Operation multi = new Multiplacation();
+                Operation div = new Division();
+                Operation sqrt = new Sqrt();
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    default:
+                        Console.WriteLine("Неизвестный оператор!");
+                        break;
+                    case 1:
+                        Console.WriteLine("Результат: " +  add, '\n');
+                        break;
+                    case 2:
+                        Console.WriteLine("Результат: " + sub, '\n');
+                        break;
+                    case 3:
+                        Console.WriteLine("Результат: " + multi, '\n');
+                        break;
+                    case 4:
+                        Console.WriteLine("Результат: " + div, '\n');  
+                        break;
+                    case 5:
+                        Console.WriteLine("Результат: " +  sqrt, "\n");
+                        break;
+                        //больше операций 
+                };
+
+                Console.Write("Вы хотите продолжить? (д/н):", '\n');
+                value = Console.ReadLine();
+            }
+            while
+
+            (value == "y" || value == "Y" || value == "д" || value == "Д");
+        }
+    }
+}
---
+++ Operations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorCsharp
+{
+
+        public abstract class Operation //шаблон операций
+        {
+            public abstract string Name { get; }
+
+            public abstract double Run(params double[] numbers);
+        }
+
+        public class Addition : Operation
+        {
+            public override string Name => "СЛОЖЕНИЕ";
+
+            public override double Run(params double[] numbers)
+            {
+                return numbers.Sum();
+            }
+        }
+
+        public class Substraction : Operation
+        {
+            public override string Name => "ВЫЧИТАНИЕ";
+
+            public override double Run(params double[] numbers)
+            {
+                return 0d;
+            }
+        }
+
+        public class Multiplacation : Operation
+        {
+            public override string Name => "УМНОЖЕНИЕ";
+
+            public override double Run(params double[] numbers)
+            {
+                return 0d;
+            }
+        }
+
+        public class Division : Operation
+        {
+            public override string Name => "ДЕЛЕНИЕ";
+
+            public override double Run(params double[] numbers)
+            {
+                return 0d;
+            }
+        }
+
+        public class Sqrt : Operation
+        {
+            public override string Name => "ВЫЧИСЛЕНИЕ КОРНЯ";
+
+            public override double Run(params double[] numbers)
+            {
+                return 0d;
+            }
+        }
+
+    }
+
