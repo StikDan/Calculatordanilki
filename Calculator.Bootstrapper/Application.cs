d5a0fa57bd35b6bf5a1368748e066b6e644d5020
StikDan <153372644+StikDan@users.noreply.github.com>
2024-06-05 23:55:44 +0300

    Add files via upload

---
+++ Calculator.Bootstrapper/Application.cs
@@ -0,0 +1,27 @@
+﻿using Calculator.Domain;
+using Calculator.Infrastructure;
+namespace Calculator.Bootstrapper;
+
+public class Application
+{
+    public Application(
+        IMenu<IOperationExecutor<double>> menu)
+    {
+        this.menu = menu;
+    }
+
+    private IMenu<IOperationExecutor<double>> menu;
+
+    public void Run()
+    {
+        while (true)
+        {
+            Console.Clear();
+            IOperationExecutor<double> operation = menu.Show().ItemSelector.Select();
+            Console.Clear();
+            Console.WriteLine($"Результат: {operation.Execute()}");
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
